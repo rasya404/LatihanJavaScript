bffa6f10312acd47ab153d80b41a647bd01736d3
unknown <rasyabuoki@gmail.com>
2024-01-18 13:48:59 -0800

    object

---
+++ object/object.js
@@ -0,0 +1,11 @@
+let buah = {
+    nama:'anggur',
+    warna:'ungu',
+    harga:'10000',
+};
+let sayur = new Object();
+sayur.nama = 'poki-poki';
+sayur.harga = 3000;
+
+console.log(buah);
+console.log(sayur);
---
+++ object/objectfungsi.js
@@ -0,0 +1,21 @@
+let hewan= {
+    nama; "kucing",
+    suara;funcition(){
+        return'meong'
+    }
+}
+
+console.log(hewan.nama);
+console.log(hewan.suara());
+
+let kalkukator ={
+    tambah: function(x,y){
+        return x+y;
+    },
+    pangkat: function(x.y){
+      retrun x**y,
+    }
+}
+
+console.log(kalkulator.tambah(7,9));
+console.log(kalkulator.pangkat(2,6));
